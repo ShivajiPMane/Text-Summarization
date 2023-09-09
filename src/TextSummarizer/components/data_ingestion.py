322d26fbf6d25a94dc77deb2506a5371ce641361
Shivaji Mane <maneshiva91@gmail.com>
2023-09-09 11:11:23 +0530

    data validation notebook added

--- src/TextSummarizer/components/data_ingestion.py
+++ src/TextSummarizer/components/data_ingestion.py
@@ -33,8 +33,8 @@ class DataIngestion:
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
 
-        #with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            #zip_ref.extractall(unzip_path)
-        
-        with py7zr.SevenZipFile(self.config.local_data_file, 'r') as zip_ref:
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+        
+        #with py7zr.SevenZipFile(self.config.local_data_file, 'r') as zip_ref:
+            #zip_ref.extractall(unzip_path)
